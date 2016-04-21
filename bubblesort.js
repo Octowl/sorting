198fd3848f6a9e2dcd8fcf444a235fcd06e5799c
Octowl <alsaff1987@gmail.com>
2016-04-21 19:54:15 -0400

    Rewrite bubblesort

--- bubblesort.js
+++ bubblesort.js
@@ -1,27 +1,23 @@
-function bubbleSort(arr) {
-    var swapCount = 0;
-
-    // var swapcount = 0;
-    // var currArr = arr.slice(0,-1);
-    // currArr.forEach(function(n,i){
-    //     if (compare(n, arr[i+1])) {
-    //         swap(arr, i);
-    //         swapcount++;
-    //     };
-    // })
-    // if (swapcount !== 0) {
-    //     var last = arr.slice(-1); // 1
-    //     arr = bubbleSort(currArr).concat(last); // [2,3].concat([1]);
-    // };
-    // return arr;
-}
-
 Array.prototype.swap = function (index) {
     index = index || 0;
-    if (index >= this.length-1) throw new Error("what are you an idiot?");
+    if (index >= this.length - 1) throw new Error("what are you an idiot?");
     var temp = this[index];
-    this[index] = this[index+1];
-    this[index+1] = temp;
+    this[index] = this[index + 1];
+    this[index + 1] = temp;
+}
+
+function bubbleSort(arr) {
+    var swapCount;
+    for (var i = arr.length - 1; i >= 0 && swapCount !== 0; i--) {
+        swapCount = 0;
+        for (var j = 0; j < i; j++) {
+            if (compare(arr[j], arr[j + 1])) {
+                arr.swap(j);
+                swapCount++;
+            }
+        }
+    }
+    return arr;
 }
 
 function compare(a, b) {
